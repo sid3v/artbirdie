7620685d229b3f8bc1e494de2c711d74b62a964b
sid3v <sudir2k@yahoo.com>
2013-08-19 15:27:30 +0000

    Added web.js and index.js

---
+++ web.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+app.use(express.logger());
+
+app.get('/', function(request, response) {
+  fs=require("fs");
+  var passhello=new Buffer(256);
+  passhello=fs.readFileSync('index.html');
+  var hello=passhello.toString();
+  response.send(hello);
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
